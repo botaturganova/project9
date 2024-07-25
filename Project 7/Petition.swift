4432173a676de4d977c9ca95125a4953e9cbc43b
botaturganova <botaturganova@gmail.com>
2024-07-25 13:48:24 +0500

    initial

---
+++ Project 7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project 7
+//
+//  Created by bota on 28.06.2024.
+//
+
+import Foundation
+
+struct Petition: Codable {
+var title: String
+var body: String
+var signatureCount: Int
+}
---
+++ Project 7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project 7
+//
+//  Created by bota on 28.06.2024.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
